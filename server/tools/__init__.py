b762d1c5f243f97c560f8609bf7b8d6b1a9275a1
pulkit2304 <pulkit2304@gmail.com>
2025-08-12 14:59:50 -0400

    feat: disable data management tools by commenting out DBFS and storage operations
    - Comment out data_management module import in __init__.py
    - Comment out load_data_tools function call in load_tools
    - Comment out entire data_management.py file content
    - Disables DBFS file operations, external locations, and storage credentials tools
    - Maintains code structure for potential future re-enabling

--- server/tools/__init__.py
+++ server/tools/__init__.py
@@ -3,7 +3,7 @@
 from .core import load_core_tools
 from .sql_operations import load_sql_tools
 from .unity_catalog import load_uc_tools
-from .data_management import load_data_tools
+# from .data_management import load_data_tools
 from .jobs_pipelines import load_job_tools
 from .workspace_files import load_workspace_tools
 from .dashboards import load_dashboard_tools
@@ -21,7 +21,7 @@ def load_tools(mcp_server):
     load_core_tools(mcp_server)
     load_sql_tools(mcp_server)
     load_uc_tools(mcp_server)
-    load_data_tools(mcp_server)
+    # load_data_tools(mcp_server)
     load_job_tools(mcp_server)
     load_workspace_tools(mcp_server)
     load_dashboard_tools(mcp_server)
